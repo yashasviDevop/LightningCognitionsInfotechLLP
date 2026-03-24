763f0add03c0b999849899d07239bf43cb0fbdd6
YPdeveloper <yashasviwpatel08@gmail.com>
2026-03-24 14:31:44 +0530

    Switch from BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // ✅ CHANGE HERE
 import App from "./App";
 import "./index.css";
 
 const loader = document.getElementById("page-loader");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>   {/* ✅ CHANGE HERE */}
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // 🔥 Remove loader AFTER React mounts
